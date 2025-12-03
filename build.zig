b9638622a0c4cab6af92dff9c68c8da5b41760e0
Filippo Rossi <filippo.rossi@datadoghq.com>
2025-12-03 22:49:54 +0100

    day2

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const DAYS = 2;
+    const DAYS = 3;
 
     inline for (0..DAYS) |day| {
         const name = std.fmt.comptimePrint("day{d}", .{day});
